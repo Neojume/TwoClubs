b00cd6efb0922729d335c65baab2c84964107f7e
Steven Laan <therealneojume@gmail.com>
2014-10-25 22:14:45 +0200

    Added license notices

--- Drivers.py
+++ Drivers.py
@@ -2,6 +2,20 @@
 Implements algorithms to find the drivers of a graph.
 A driver is a node whose ego-network is not contained in
 the ego-network of any other node.
+
+Copyright (C) 2012  Steven Laan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
 '''
 
 import string
